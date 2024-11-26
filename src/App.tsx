6bb57ab89cdb8c58bae34aa12fb82fffbb3efc78
souph1s <sophiamperez@hotmail.com>
2024-11-26 20:26:31 -0300

    feat: create project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <header>Apple TV+</header>
+      <main>
+        <div>Hero Component</div>
+        <div>USPS</div>
+        <div>3 columns</div>
+        <div>Carousel With Posters</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
