210441dc77f2be19771711d36d5cb32a9de0ffca
souph1s <sophiamperez@hotmail.com>
2024-12-07 17:07:35 -0300

    feat: add icons on the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { Search, ShoppingBag } from 'lucide-react';
 import { Button } from './button';
 import { Container } from './container';
 
@@ -5,19 +6,32 @@ export const Header = () => {
   return (
     <>
       <header className="bg-backgroundContrast relative z-20 text-white">
-        <Container className="flex min-h-[--header-row-height] items-center">
+        <Container className="flex min-h-[--header-row-height] justify-around items-center">
           <a
             href="/"
             className="-ml-6 flex h-[--header-row-height] items-center px-6 text-xl"
           >
              <span className="sr-only">Back to homepage</span>
           </a>
+          <p>Store</p>
+          <p>Mac</p>
+          <p>iPad</p>
+          <p>iPhone</p>
+          <p>Watch</p>
+          <p>Vision</p>
+          <p>AirPods</p>
+          <p>TV & Home</p>
+          <p>Entertainment</p>
+          <p>Accessories</p>
+          <p>Support</p>
+          <Search className="h-6 w-6" />
+          <ShoppingBag className="w-6 h-6" />
         </Container>
       </header>
       <div className="bg-backgroundContrast sticky top-0 z-20 text-white">
-        <Container className="flex min-h-[--header-row-height] items-center justify-between">
-          <p className="text-xl font-semibold">Apple TV+</p>
-          <Button size="small">Stream now</Button>
+        <Container className="flex h-24 items-center justify-between">
+          <p className="text-3xl font-semibold">Apple TV+</p>
+          <Button size="large">Stream now</Button>
         </Container>
       </div>
     </>
